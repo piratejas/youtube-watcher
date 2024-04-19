2dc3480bd7ba929327ef218670821bb7402938b5
piratejas <piratejas@gmail.com>
2024-04-19 11:29:32 +0100

    fetch formatted video data

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from pprint import pformat
 import sys
 import requests
 from config import config
@@ -21,6 +22,22 @@ def fetch_playlist_items_page(google_api_key, youtube_playlist_id, page_token=No
 	return payload
 
 
+def fetch_videos_page(google_api_key, video_id, page_token=None):
+	response = requests.get(
+		"https://www.googleapis.com/youtube/v3/videos",
+		params={
+			"key": google_api_key,
+			"id": video_id,
+			"part": "snippet, statistics",
+			"pageToken": page_token,
+		},
+	)
+	payload = json.loads(response.text)
+	logging.debug("Got response: %s", payload)
+
+	return payload
+
+
 def fetch_playlist_items(google_api_key, youtube_playlist_id, page_token=None):
 	payload = fetch_playlist_items_page(google_api_key, youtube_playlist_id, page_token)
 
@@ -32,14 +49,37 @@ def fetch_playlist_items(google_api_key, youtube_playlist_id, page_token=None):
 		yield from fetch_playlist_items(google_api_key, youtube_playlist_id, next_page_token)
 
 
+def fetch_videos(google_api_key, video_id, page_token=None):
+	payload = fetch_videos_page(google_api_key, video_id, page_token)
+
+	yield from payload["items"]
+
+	next_page_token = payload.get("nextPageToken")
+
+	if next_page_token is not None:
+		yield from fetch_videos(google_api_key, video_id, next_page_token)
+
+
+def summarize_video(video):
+
+	return {
+		"video_id": video["id"],
+		"title": video["snippet"]["title"],
+		"views": int(video["statistics"].get("viewCount", 0)),
+		"likes": int(video["statistics"].get("likeCount", 0)),
+		"comments": int(video["statistics"].get("commentCount", 0)),
+	}
+
+
 def main():
 	logging.info("Starting app...")
 	google_api_key = config["google_api_key"]
 	youtube_playlist_id = config["youtube_playlist_id"]
 	
 	for video_item in fetch_playlist_items(google_api_key, youtube_playlist_id):
-		logging.info("Got video item: %s", video_item)
-	
+		video_id = video_item["contentDetails"]["videoId"]
+		for video in fetch_videos(google_api_key, video_id):
+			logging.info("Got video: %s", pformat(summarize_video(video)))
 
 
 if __name__ == "__main__":
