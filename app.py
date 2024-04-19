fcdc4a33cae63e3e7dbd07e83063d171baa034a7
piratejas <piratejas@gmail.com>
2024-04-19 11:08:51 +0100

    added generator to depaginate payload

--- app.py
+++ app.py
@@ -12,23 +12,36 @@ def fetch_playlist_items_page(google_api_key, youtube_playlist_id, page_token=No
 			"key": google_api_key,
 			"playlistId": youtube_playlist_id,
 			"part": "contentDetails",
+			"pageToken": page_token,
 		},
 	)
 	payload = json.loads(response.text)
-	
 	logging.debug("Got response: %s", payload)
-	
+
 	return payload
+
+
+def fetch_playlist_items(google_api_key, youtube_playlist_id, page_token=None):
+	payload = fetch_playlist_items_page(google_api_key, youtube_playlist_id, page_token)
+
+	yield from payload["items"]
+
+	next_page_token = payload.get("nextPageToken")
+
+	if next_page_token is not None:
+		yield from fetch_playlist_items(google_api_key, youtube_playlist_id, next_page_token)
+
 
 def main():
 	logging.info("Starting app...")
 	google_api_key = config["google_api_key"]
 	youtube_playlist_id = config["youtube_playlist_id"]
 	
-	fetch_playlist_items_page(google_api_key, youtube_playlist_id)
+	for video_item in fetch_playlist_items(google_api_key, youtube_playlist_id):
+		logging.info("Got video item: %s", video_item)
 	
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     sys.exit(main())
