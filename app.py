23b2654c5a14d5b07e593baefebbd0432f7f62e4
piratejas <piratejas@gmail.com>
2024-04-19 10:45:07 +0100

    added params; refactored fetch function

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
+import json
 import logging
 import sys
 import requests
 from config import config
 
 
+def fetch_playlist_items_page(google_api_key, youtube_playlist_id, page_token=None):
+	response = requests.get(
+		"https://www.googleapis.com/youtube/v3/playlistItems",
+		params={
+			"key": google_api_key,
+			"playlistId": youtube_playlist_id,
+			"part": "contentDetails",
+		},
+	)
+	payload = json.loads(response.text)
+	
+	logging.debug("Got response: %s", payload)
+	
+	return payload
+
 def main():
-    logging.info("Starting app...")
-
-    response = requests.get(
-        "https://www.googleapis.com/youtube/v3/playlistItems",
-        params={
-            "key": config["google_api_key"],
-        },
-    )
-
-    logging.debug("Got response: %s", response.text)
+	logging.info("Starting app...")
+	google_api_key = config["google_api_key"]
+	youtube_playlist_id = config["youtube_playlist_id"]
+	
+	fetch_playlist_items_page(google_api_key, youtube_playlist_id)
+	
 
 
 if __name__ == "__main__":
